b25b7b4d2c303637d04192c1703f954b262d40b1
Josh Kunz <joshkunz@me.com>
2015-02-21 01:03:10 -0700

    Adds split on impl.

--- Common.hs
+++ Common.hs
@@ -1,5 +1,15 @@
 module Common where
 
+-- Lower precedence reverse application operator
+(|>) :: a -> (a -> b) -> b
+(|>) x f = f x
+infixl 0 |>
+
+-- Higher precidence right associative application operator
+($|) :: (a -> b) -> a -> b
+($|) f x = f x
+infixr 1 $|
+
 not_implemented s = error $ "not implemented: " ++ s
 unreachable s = error $ "unreachable: " ++ s
 
@@ -7,7 +17,9 @@ joinWithSep :: [a] -> [[a]] -> [a]
 joinWithSep sep [] = []
 joinWithSep sep (x:xs) = x ++ sep ++ (joinWithSep sep xs)
 
-(|>) :: a -> (a -> b) -> b
-(|>) x f = f x
-
-infixl 1 |>
+splitOn :: (Eq a) => a -> [a] -> [[a]]
+splitOn with l = 
+    let fsplit (x:xs) i = 
+            if i == with then (reverse x : xs) 
+            else ((i : x) : xs)
+    in foldl fsplit [[]] l |> reverse
